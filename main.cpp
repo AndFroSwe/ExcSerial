c08e7508ec8894f9e41889366996644db3776097
andfro <andfro@protonmail.com>
2025-08-31 09:35:26 +0200

    Print status. Better messages.
    Also send errors to cerr.
    Use busy wait loop with yield to
    allow for <16 ms sleep.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
- * @brief Windows serial application for sending pulse dir control data via serial
- * to Excelsior ECU.
+ * @brief Windows serial application for sending pulse dir control data via
+ * serial to Excelsior ECU.
  *
  * Simple app that sends serial data to test pulsedir on a windows computer.
  * Usage: excserial COM3 10 500
@@ -32,9 +32,28 @@ BOOL WINAPI CtrlHandler(DWORD ctrlType) {
   }
 }
 
+inline std::string error_string(DWORD error_code) {
+  char *buffer = nullptr;
+  const DWORD len = ::FormatMessageA(
+      FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
+          FORMAT_MESSAGE_IGNORE_INSERTS,
+      nullptr, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      reinterpret_cast<char *>(&buffer), 0, nullptr);
+
+  if (len == 0 || buffer == nullptr)
+    return "Unknown error (" + std::to_string(error_code) + ")";
+
+  std::string result{buffer};
+  ::LocalFree(buffer);
+  // Remove trailing CRLF
+  while (!result.empty() && (result.back() == '\r' || result.back() == '\n'))
+    result.pop_back();
+  return result;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 4) {
-    std::cout << "Usage: winserial COM3 10 500 [Pulses with 10 pulses "
+    std::cout << "Usage: excserial COM3 10 500 [Pulses with 10 pulses "
                  "alternating +/- at 500 Hz]"
               << std::endl;
     return EXIT_SUCCESS;
@@ -43,55 +62,65 @@ int main(int argc, char *argv[]) {
   std::cout << "Starting excserial program..." << std::endl;
 
   // Validate the input
-  int n;
+  int n; // Number to sent each iteration
   {
     std::stringstream ss(argv[2]);
     ss >> n;
     if (!ss) {
-      std::cout << std::format("Can't convert arg {} to number!",
+      std::cerr << std::format("Can't convert arg {} to number!",
                                std::string_view(argv[2]))
                 << std::endl;
       return EXIT_FAILURE;
     }
   }
 
-  int f;
+  int f; // Frequency to send
   {
     std::stringstream ss(argv[3]);
     ss >> f;
     if (!ss) {
-      std::cout << std::format("Can't convert arg {} to number!",
+      std::cerr << std::format("Can't convert arg {} to number!",
                                std::string_view(argv[2]))
                 << std::endl;
       return EXIT_FAILURE;
     }
     if (f > 1000) {
-      std::cout << "Frequency cant be bigger than 1000" << std::endl;
+      std::cerr << "Frequency cant be bigger than 1000" << std::endl;
       return EXIT_FAILURE;
     }
   }
   std::chrono::milliseconds sleep_time_ms{1000 / f};
 
   // Bind to the com port
-  std::string comport{argv[1]}; // Name of the com port
+  std::string_view comport{argv[1]}; // Name of the com port
   const CHAR *pcCommPort = TEXT(argv[1]);
   HANDLE hCom = CreateFile(pcCommPort, GENERIC_READ | GENERIC_WRITE, 0, nullptr,
                            OPEN_EXISTING, 0, nullptr);
 
   if (hCom == INVALID_HANDLE_VALUE) {
-    std::cout << std::format("CreateFile failed with error {}", GetLastError())
+    std::cerr << std::format("CreateFile failed with error: {}",
+                             error_string(GetLastError()))
               << std::endl;
     return EXIT_FAILURE;
   }
 
-  // Initialize DCB structure
+  // Validation done
+  // Handle ctrl+c
+  if (!SetConsoleCtrlHandler(CtrlHandler, TRUE)) {
+    std::cerr << "Failed to set control handler: "
+              << error_string(GetLastError()) << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Initialize DCB structure for com port
   DCB dcb;
   SecureZeroMemory(&dcb, sizeof(DCB));
   dcb.DCBlength = sizeof(DCB);
 
   // Get current settings
   if (!GetCommState(hCom, &dcb)) {
-    std::cout << std::format("GetCommState filed with error {}", GetLastError())
+    std::cerr << std::format("GetCommState filed with error {}",
+                             error_string(GetLastError()))
               << std::endl;
   }
 
@@ -102,26 +131,60 @@ int main(int argc, char *argv[]) {
   dcb.StopBits = ONESTOPBIT;
   std::cout << "Serial port successfully configured!" << std::endl;
 
-  // Start sending
-  std::cout << "Sending [+/-] " << n << " to " << comport << " with " <<
-    f << "Hz (" << sleep_time_ms.count() << " ms..." << std::endl;
+  COMMTIMEOUTS timeouts = {0};
+  timeouts.ReadIntervalTimeout = 50;
+  timeouts.ReadTotalTimeoutConstant = 10;
+  timeouts.ReadTotalTimeoutMultiplier = 10;
+  timeouts.WriteTotalTimeoutConstant = 50;
+  timeouts.WriteTotalTimeoutMultiplier = 10;
+  if (!SetCommTimeouts(hCom, &timeouts)) {
+    CloseHandle(hCom);
+    std::cerr << "Could not set timeouts with error: "
+              << error_string(GetLastError()) << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Setup send loop
+  constexpr auto status_print_time = 2s;
+  unsigned int messages_sent = 0;
+  auto last_send_time = std::chrono::steady_clock::now();
+  auto last_print_time = last_send_time;
+
+  std::cout << "Sending [+/-] " << n << " to " << comport << " with " << f
+            << "Hz (" << sleep_time_ms.count() << " ms)..." << std::endl;
+
+  // Start the loop
   while (!gStopRequested) {
+    // Busy wait loop since windows can't do sub 16 ms sleep with chrono
+    while (std::chrono::steady_clock::now() - last_send_time < sleep_time_ms) {
+      Sleep(0); // Yield CPU
+    }
+    const auto now = std::chrono::steady_clock::now();
+    last_send_time = now;
+
+    // Try to send
     std::string msg = std::format("#{},{},{},{};", n, n, n, n);
     DWORD bytesWritten = 0;
     if (!WriteFile(hCom, msg.c_str(), static_cast<DWORD>(msg.size()),
                    &bytesWritten, nullptr)) {
-      std::cout << "Failed to write to " << comport
-                << "with error: " << std::to_string(GetLastError())
-                << std::endl;
+      std::cerr << "Failed to write to " << comport
+                << " with error: " << error_string(GetLastError()) << std::endl;
       CloseHandle(hCom);
       return EXIT_FAILURE;
     } else {
       n *= -1; // Flip n to alternate
+      messages_sent++;
     }
-    std::this_thread::sleep_for(sleep_time_ms);
+
+    // Status print
+    if (now - last_print_time > status_print_time) {
+      std::cout << '\r' << std::string(120, ' ');
+      std::cout << "\rMessages sent: " << messages_sent << std::flush;
+      last_print_time = now;
+    }
   }
 
-  std::cout << "Got ctrl+c, exiting..." << std::endl;
+  std::cout << std::endl << "Got ctrl+c, exiting..." << std::endl;
 
   CloseHandle(hCom);
 
